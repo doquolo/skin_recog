06bdb986e54d74ea6cfb59e1cccf2f721a61923d
doquolo <doquolo123@outlook.com>
2024-08-28 11:36:36 +0700

    stripped down to core

--- README.md
+++
@@ -1,1 +0,0 @@
-Tự tạo thêm Folder segments và test (có 4 ảnh frontend)
--- web/recog.py
+++ web/recog.py
@@ -1,124 +1,127 @@
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from flask import Flask, render_template, request, send_file, jsonify
 from PIL import Image, ImageOps
 import numpy as np
 from skimage.segmentation import slic, mark_boundaries
 from skimage.util import img_as_float
 from tensorflow import keras
-
-import matplotlib
+import numpy as np
 import io
-matplotlib.use('Agg')  # Sử dụng backend Agg để tránh lỗi RuntimeError
-
+import os
+import requests
+import skimage.io
+from flask import Flask, render_template, request, send_file, jsonify
 
 app = Flask(__name__)
 
-# Load the pre-trained model
-# model = keras.models.load_model('models/all.h5')
-# Load the labels
-# class_names = ['mụn trứng cá', 'viêm da', 'nấm da', 'khỏe mạnh']
+MODEL_FOLDER = os.path.join(app.root_path, 'models')
 
-
-def predict_with_model(img, seg, selection):
+def predict_with_model(img, selection):
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
     image = img.convert("RGB")
-
     size = (224, 224)
     image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
-
     image_array = np.asarray(image)
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-
     data[0] = normalized_image_array
-
+    
     # load model
+    if selection == ['acne', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/a-h.h5')
+    elif selection == ['der', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/d-h.h5')
+    elif selection == ['rw', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/r-h.h5')
+    elif selection == ['sc', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/c-h.h5')
+    elif selection == ['acne', 'der', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/a-d-h.h5')
+    elif selection == ['acne', 'rw', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/a-r-h.h5')
+    elif selection == ['acne', 'sc', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/a-c-h.h5')
+    elif selection == ['der', 'rw', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/d-r-h.h5')
+    elif selection == ['der', 'sc', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/d-c-h.h5')
+    elif selection == ['rw', 'sc', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/r-c-h.h5')
+    elif selection == ['acne', 'der', 'rw', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/a-d-r-h.h5')
+    elif selection == ['acne', 'der', 'sc', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/a-d-c-h.h5')
+    elif selection == ['acne', 'rw', 'sc', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/a-r-c-h.h5')
+    elif selection == ['der', 'rw', 'sc', 'healthy']:
+        model = keras.models.load_model(f'{MODEL_FOLDER}/d-r-c-h.h5')
+    elif selection == ['acne', 'der', 'rw', 'sc', 'healthy'] or selection == ['healthy']:
+        selection = ['acne', 'der', 'rw', 'sc', 'healthy']
+        model = keras.models.load_model(f'{MODEL_FOLDER}/a-d-r-c-h.h5')
+    
 
-    if selection == ['mụn trứng cá', 'khỏe mạnh']:
-        model = keras.models.load_model('models/acne.h5')
-    elif selection == ['viêm da', 'khỏe mạnh']:
-        model = keras.models.load_model('models/der.h5')
-    elif selection == ['nấm da', 'khỏe mạnh']:
-        model = keras.models.load_model('models/rw.h5')
-    elif selection == ['mụn trứng cá', 'viêm da', 'khỏe mạnh']:
-        model = keras.models.load_model('models/acne-der.h5')
-    elif selection == ['mụn trứng cá', 'nấm da', 'khỏe mạnh']:
-        model = keras.models.load_model('models/acne-rw.h5')
-    elif selection == ['viêm da', 'nấm da', 'khỏe mạnh']:
-        model = keras.models.load_model('models/der-rw.h5')
-    elif selection == ['mụn trứng cá', 'khỏe mạnh']:
-        model = keras.models.load_model('models/acne.h5')
-    elif selection == ['viêm da', 'khỏe mạnh']:
-        model = keras.models.load_model('models/der.h5')
-    elif selection == ['nấm da', 'khỏe mạnh']:
-        model = keras.models.load_model('models/rw.h5')
-
-    elif selection == ['mụn trứng cá', 'viêm da', 'nấm da', 'khỏe mạnh'] or selection == ['khỏe mạnh']:
-        selection = ['mụn trứng cá', 'viêm da', 'nấm da', 'khỏe mạnh']
-        model = keras.models.load_model('models/all.h5')
-
+    skin_disease = []
+    for i in selection: 
+        if i == "acne": skin_disease.append("mụn trứng cá")
+        if i == "der": skin_disease.append("viêm da")
+        if i == "rw": skin_disease.append("nấm da")
+        if i == "sc": skin_disease.append("ung thư da")
+        if i == "healthy": skin_disease.append("khỏe mạnh")
+    
     prediction = model.predict(data)
     index = np.argmax(prediction)
-    selection = selection[index]
+    selection = skin_disease[index]
+    #selection = selection[index]
     confidence_score = prediction[0][index]
 
     return {"class_name": selection, "confidence": confidence_score}
 
 
 def recognize(img, selection):
-    pathSave = 'segments/'
-
     skin_image = Image.fromarray(img).resize((224, 224))
     segments = slic(img_as_float(skin_image), n_segments=5, sigma=5)
 
-    fig = Figure()  # Tạo đối tượng Figure mới
-    canvas = FigureCanvas(fig)  # Tạo canvas cho Figure
-
+    fig = Figure()
+    canvas = FigureCanvas(fig)
     ax = fig.add_subplot(1, 1, 1)
-    ax.imshow(mark_boundaries(img_as_float(skin_image),
-              segments, background_label=0, mode='thick'))
+    ax.imshow(mark_boundaries(img_as_float(skin_image), segments, background_label=0, mode='thick'))
     ax.axis("off")
+    
+    canvas.draw()
+    superpixel_img = np.frombuffer(canvas.tostring_rgb(), dtype=np.uint8)
+    superpixel_img = superpixel_img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    
+    # Crop phần trung tâm 250x250 pixels
+    height, width, _ = superpixel_img.shape
+    center_x, center_y = width // 2, height // 2
+    crop_size = 400
 
-    canvas.draw()  # Vẽ canvas
+    # Tính toán tọa độ của vùng crop
+    start_x = max(center_x - crop_size // 2, 0)
+    start_y = max(center_y - crop_size // 2, 0)
+    end_x = min(start_x + crop_size, width)
+    end_y = min(start_y + crop_size, height)
 
-    # Lưu figure vào file ảnh
-    canvas.print_png("test/.superpixels.png")
-    image = Image.open('test/.superpixels.png')
-    image.convert('RGB').save('test/.superpixels.jpg', 'JPEG')
+    # Crop ảnh
+    superpixel_img = superpixel_img[start_y:end_y, start_x:end_x]
 
-    res = []
-
-    segNum = 0
-    for (i, segVal) in enumerate(np.unique(segments)):
-        temp_i = i
+    segment_results = []
+    for i, segVal in enumerate(np.unique(segments)):
         mask = np.zeros(skin_image.size[:2], dtype="uint8")
         mask[segments == segVal] = 255
         extracted_mask = np.stack(np.nonzero(mask), axis=-1)
         im = Image.fromarray(mask)
-        cropped_segment = im.crop((min(extracted_mask[:, 1]), min(
-            extracted_mask[:, 0]), max(extracted_mask[:, 1]), max(extracted_mask[:, 0])))
-        cropped_img_segment = skin_image.crop((min(extracted_mask[:, 1]), min(
-            extracted_mask[:, 0]), max(extracted_mask[:, 1]), max(extracted_mask[:, 0])))
+        cropped_segment = im.crop((min(extracted_mask[:, 1]), min(extracted_mask[:, 0]), max(extracted_mask[:, 1]), max(extracted_mask[:, 0])))
+        cropped_img_segment = skin_image.crop((min(extracted_mask[:, 1]), min(extracted_mask[:, 0]), max(extracted_mask[:, 1]), max(extracted_mask[:, 0])))
         temp = np.array(cropped_segment) / 255
         output = np.array(cropped_img_segment)
 
-        for i in range(3):
-            final_result = np.multiply(
-                temp, (np.array(cropped_img_segment)[:, :, i]))
-            output[:, :, i] = final_result
+        for j in range(3):
+            final_result = np.multiply(temp, (np.array(cropped_img_segment)[:, :, j]))
+            output[:, :, j] = final_result
         final_seg = Image.fromarray(output)
 
-        out = predict_with_model(final_seg, temp_i, selection)
+        out = predict_with_model(final_seg, selection)
+        segment_results.append({"segment": i, "prediction": out['class_name'], "confidence": str(round(out['confidence']*100)), "image": output})
 
-        res.append({"segment": temp_i, "prediction": out['class_name'], "confidence": str(
-            round(out['confidence']*100)), "segment_data": mask})
-        Image.fromarray(mask).save(
-            f"segments/seg-{temp_i}_pred-{out['class_name']}_conf-{round(out['confidence'], 3)}.jpg")
-
-        print(
-            f"seg-{temp_i}_pred-{out['class_name']} \nConfidence: {round(out['confidence'], 3)}")
-
-        print(selection)
-
-    return res
+    return {"superpixel": superpixel_img, "segments": segment_results}
